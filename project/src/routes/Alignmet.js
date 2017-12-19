cc038b07f4110d248afeeaa8ee4ba75de9bb6ced
sunzhiyue <sunzhiue@outlook.com>
2017-12-19 21:15:54 +0800

    添加组件

--- project/src/routes/Alignmet.js
+++ project/src/routes/Alignmet.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Segment, Image } from 'semantic-ui-react';
+import { Segment, Image, Icon } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import Cardcontent from '../components/Cardcontent/Cardcontent';
 import styles from './aignment.less';
 
 const HeaderExampleTextAlignment = () => (
@@ -32,16 +33,41 @@ const HeaderExampleTextAlignment = () => (
       </Segment>
       <div className={styles.primary}>
         <div className={styles.post}>
-          <a href="" >
-            <img alt="图片" src="http://isujin.com/wp-content/themes/Diaspora/timthumb/timthumb.php?src=http://isujin.com/wp-content/uploads/2017/09/wallhaven-241627.jpg" size="big" />
-          </a>
+          <div className={styles.img}>
+            <a href="" ><img alt="图片" src="http://isujin.com/wp-content/themes/Diaspora/timthumb/timthumb.php?src=http://isujin.com/wp-content/uploads/2017/09/wallhaven-241627.jpg" size="big" /></a>
+          </div>
           <div className={styles.else}>
             <p>九月 19，2017</p>
-            <h3><a href="">我是占位符</a></h3>
-            <p>占位符占位符占位符占位符占位符占位符占位符占位符占位符占位符占位符占位符占位符占位符。。。</p>
+            <h3><a href="">我终于瘦下来了，却不再喜欢你</a></h3>
+            <p className={styles.content}>01 许桐最大的毛病就是贪吃。
+             她尤其喜欢吃甜<br />食，精致的蛋糕、松软的面包、酥脆的饼干都是她<br />的心头好...</p>
+            <div className={styles.icon}>
+              <Icon name="edit" />111
+              <Icon name="unhide" />222
+              <Icon name="heart" />10
+            </div>
           </div>
         </div>
       </div>
+      <div className={styles.primary}>
+        <div className={styles.post}>
+          <div className={styles.img}>
+            <a href="" ><img alt="图片" src="http://isujin.com/wp-content/themes/Diaspora/timthumb/timthumb.php?src=http://isujin.com/wp-content/uploads/2017/09/wallhaven-241627.jpg" size="big" /></a>
+          </div>
+          <div className={styles.else}>
+            <p>九月 19，2017</p>
+            <h3><a href="">我终于瘦下来了，却不再喜欢你</a></h3>
+            <p className={styles.content}>01 许桐最大的毛病就是贪吃。
+             她尤其喜欢吃甜<br />食，精致的蛋糕、松软的面包、酥脆的饼干都是她<br />的心头好...</p>
+            <div className={styles.icon}>
+              <Icon color="grey" name="edit" />111
+              <Icon color="grey" name="unhide" />222
+              <Icon color="grey" name="heart" />10
+            </div>
+          </div>
+        </div>
+      </div>
+      <Cardcontent />
     </div>
   </div>
 );
