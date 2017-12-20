89c8f5a2b4a43c12ae6ea90577f477b290cd024e
sunzhiyue <sunzhiue@outlook.com>
2017-12-20 21:05:29 +0800

    组件

--- project/src/routes/Alignmet.js
+++ project/src/routes/Alignmet.js
@@ -4,6 +4,14 @@ import 'semantic-ui-css/semantic.min.css';
 import Cardcontent from '../components/Cardcontent/Cardcontent';
 import styles from './aignment.less';
 
+// const panels = [{
+//   title: '说句再见吧，少年',
+//   content: '1 毕业之后，我和大学的很多同学都留在了这座不大的城市里，两个室友在学校附近租了一套小资豪华房，我...',
+// }, {
+//   title: '说句再见吧，少',
+//   content: '1 毕业之后，我和大学的很多同学都留在了这座不大的城市里，两个室友在学校附近租了一套小资豪华房，我...',
+// }];
+
 const HeaderExampleTextAlignment = () => (
   <div>
     <div className={styles.nav} >
@@ -31,7 +39,7 @@ const HeaderExampleTextAlignment = () => (
           </div>
         </div>
       </Segment>
-      <div className={styles.primary}>
+      <Segment className={styles.primary}>
         <div className={styles.post}>
           <div className={styles.img}>
             <a href="" ><img alt="图片" src="http://isujin.com/wp-content/themes/Diaspora/timthumb/timthumb.php?src=http://isujin.com/wp-content/uploads/2017/09/wallhaven-241627.jpg" size="big" /></a>
@@ -45,6 +53,24 @@ const HeaderExampleTextAlignment = () => (
               <Icon name="edit" />111
               <Icon name="unhide" />222
               <Icon name="heart" />10
+            </div>
+          </div>
+        </div>
+      </Segment>
+      <div className={styles.primary}>
+        <div className={styles.post}>
+          <div className={styles.img}>
+            <a href="" ><img alt="图片" src="http://isujin.com/wp-content/themes/Diaspora/timthumb/timthumb.php?src=http://isujin.com/wp-content/uploads/2017/09/wallhaven-241627.jpg" size="big" /></a>
+          </div>
+          <div className={styles.else}>
+            <p>九月 19，2017</p>
+            <h3><a href="">我终于瘦下来了，却不再喜欢你</a></h3>
+            <p className={styles.content}>01 许桐最大的毛病就是贪吃。
+             她尤其喜欢吃甜<br />食，精致的蛋糕、松软的面包、酥脆的饼干都是她<br />的心头好...</p>
+            <div className={styles.icon}>
+              <Icon color="grey" name="edit" />111
+              <Icon color="grey" name="unhide" />222
+              <Icon color="grey" name="heart" />10
             </div>
           </div>
         </div>
@@ -67,6 +93,13 @@ const HeaderExampleTextAlignment = () => (
           </div>
         </div>
       </div>
+      <Cardcontent />
+      <Cardcontent />
+      <Cardcontent />
+      <Cardcontent />
+      <Cardcontent />
+      <Cardcontent />
+      <Cardcontent />
       <Cardcontent />
     </div>
   </div>
